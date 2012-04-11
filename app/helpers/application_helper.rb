38cc22a49b051ace11d741a6ba780913f8499c69
Fernando <alvarez.calleja.fernando@gmail.com>
2012-04-12 01:43:47 +0200

    Style OK and currency fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def currency_euro(item)
+    number_to_currency(item, :unit => "&euro;", :separator => ",", :delimiter => "", :format => "%n %u")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 Pattern.delete_all
 
 Pattern.create(:title => 'Ethnic',
-:description =>
-%{<p>
-Feel the shapes of our origins.
-</p>},
-:image_url => '/images/pattern01.png',
-:price => 0.99)
+  :description =>
+    %{<p>
+      Feel the shapes of our origins.
+    </p>},
+  :image_url => '/images/pattern01.png',
+  :price => 0.99)
 
-Pattern.create(:title => 'Ethnic Africa',
-:description =>
-%{<p>
-The heart of Africa wherever you want.
-</p>},
-:image_url => '/images/pattern02.png',
-:price => 0.99)
+Pattern.create(:title => 'Africa',
+  :description =>
+    %{<p>
+      The heart of Africa wherever you want.
+    </p>},
+  :image_url => '/images/pattern02.png',
+  :price => 0.99)
 
